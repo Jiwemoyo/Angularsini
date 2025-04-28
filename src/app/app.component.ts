ba206e7860798d207c5a2210065c44d251f1cea2
Aless4ndr0 <jiwemoyo@gmail.com>
2025-04-28 16:33:23 -0500

    otro salto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormsModule,ReactiveFormsModule,FormControl,FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,FormsModule],
+  imports: [RouterOutlet,RouterLink,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Practicas';
   fruta = ''
+  mostrarFruta() {
+    alert(this.fruta)
+  }
+  profileForm = new FormGroup({
+    name: new FormControl('Isabel',Validators.required),
+    email: new FormControl('',[Validators.required, Validators.email]),
+  })
+
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
+
 }
